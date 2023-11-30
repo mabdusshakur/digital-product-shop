d68b7ecb2af656ed2a519e98a0e5d4a9a6c9c257
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-11-30 22:16:35 +0600

    column added

--- database/migrations/2023_11_30_135623_create_abouts_table.php
+++ database/migrations/2023_11_30_135623_create_abouts_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('abouts', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->longText('description');
             $table->timestamps();
         });
     }
--- database/migrations/2023_11_30_135630_create_contacts_table.php
+++ database/migrations/2023_11_30_135630_create_contacts_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('contacts', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('subject');
+            $table->longText('message');
             $table->timestamps();
         });
     }
