a5a7949ff0568b3d45b1074e15536515136c62e5
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-11-30 18:11:21 +0600

    basic User/HomeComponent added

---
+++ app/Livewire/User/HomeComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\User;
+
+use Livewire\Component;
+
+class HomeComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.user.home-component');
+    }
+}
