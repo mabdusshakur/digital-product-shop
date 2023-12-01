82c9f938d7cc28f694bcaa02c3778ad621114637
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-01 19:08:10 +0600

    all-create-update products Component added

--- resources/views/livewire/admin/all-products-component.blade.php
+++ resources/views/livewire/admin/all-products-component.blade.php
@@ -1,3 +1,142 @@
 <div>
-    {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
+    <div class="card">
+        <div class="card-header py-3">
+            <div class="row align-items-center m-0">
+                <div class="col-md-3 col-12 me-auto mb-md-0 mb-3">
+                    <select class="form-select">
+                        <option>All category</option>
+                        <option>Fashion</option>
+                        <option>Electronics</option>
+                        <option>Furniture</option>
+                        <option>Sports</option>
+                    </select>
+                </div>
+                <div class="col-md-2 col-6">
+                    <select class="form-select">
+                        <option>Status</option>
+                        <option>Active</option>
+                        <option>Disabled</option>
+                        <option>Show all</option>
+                    </select>
+                </div>
+            </div>
+        </div>
+        <div class="card-body">
+
+            <div class="table-responsive">
+                <table class="table align-middle table-striped">
+                    <thead class="table-light">
+                        <tr>
+                            <th>Name</th>
+                            <th>Regular Price</th>
+                            <th>Discount Price</th>
+                            <th>Status</th>
+                            <th>Stocks</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td class="productlist">
+                                <a class="d-flex align-items-center gap-2" href="#">
+                                    <div class="product-box">
+                                        <img src="assets/images/products/01.png" alt="">
+                                    </div>
+                                    <div>
+                                        <h6 class="mb-0 product-title">Men White Polo T-shirt</h6>
+                                    </div>
+                                </a>
+                            </td>
+                            <td><span>$18.00</span></td>
+                            <td><span>$10.00</span></td>
+                            <td><span class="badge rounded-pill bg-success">Active</span></td>
+                            <td><span>44</span></td>
+                            <td>
+                                <div class="d-flex align-items-center gap-3 fs-6">
+                                    <a href="javascript:;" class="text-primary" data-bs-toggle="tooltip"
+                                        data-bs-placement="bottom" title="" data-bs-original-title="View detail"
+                                        aria-label="Views"><i class="bi bi-eye-fill"></i></a>
+                                    <a href="javascript:;" class="text-warning" data-bs-toggle="tooltip"
+                                        data-bs-placement="bottom" title="" data-bs-original-title="Edit info"
+                                        aria-label="Edit"><i class="bi bi-pencil-fill"></i></a>
+                                    <a href="javascript:;" class="text-danger" data-bs-toggle="tooltip"
+                                        data-bs-placement="bottom" title="" data-bs-original-title="Delete"
+                                        aria-label="Delete"><i class="bi bi-trash-fill"></i></a>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="productlist">
+                                <a class="d-flex align-items-center gap-2" href="#">
+                                    <div class="product-box">
+                                        <img src="assets/images/products/04.png" alt="">
+                                    </div>
+                                    <div>
+                                        <h6 class="mb-0 product-title">Yellow Winter Jacket for Men</h6>
+                                    </div>
+                                </a>
+                            </td>
+                            <td><span>$18.00</span></td>
+                            <td><span>No Discount Added</span></td>
+                            <td><span class="badge rounded-pill bg-dark">On Flash</span></td>
+                            <td><span>22</span></td>
+                            <td>
+                                <div class="d-flex align-items-center gap-3 fs-6">
+                                    <a href="javascript:;" class="text-primary" data-bs-toggle="tooltip"
+                                        data-bs-placement="bottom" title="" data-bs-original-title="View detail"
+                                        aria-label="Views"><i class="bi bi-eye-fill"></i></a>
+                                    <a href="javascript:;" class="text-warning" data-bs-toggle="tooltip"
+                                        data-bs-placement="bottom" title="" data-bs-original-title="Edit info"
+                                        aria-label="Edit"><i class="bi bi-pencil-fill"></i></a>
+                                    <a href="javascript:;" class="text-danger" data-bs-toggle="tooltip"
+                                        data-bs-placement="bottom" title="" data-bs-original-title="Delete"
+                                        aria-label="Delete"><i class="bi bi-trash-fill"></i></a>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="productlist">
+                                <a class="d-flex align-items-center gap-2" href="#">
+                                    <div class="product-box">
+                                        <img src="assets/images/products/05.png" alt="">
+                                    </div>
+                                    <div>
+                                        <h6 class="mb-0 product-title">Men Sports Shoes Nike</h6>
+                                    </div>
+                                </a>
+                            </td>
+                            <td><span>$18.00</span></td>
+                            <td><span>No Discount Added</span></td>
+                            <td><span class="badge rounded-pill bg-warning">Out Of Stock</span></td>
+                            <td><span>0</span></td>
+                            <td>
+                                <div class="d-flex align-items-center gap-3 fs-6">
+                                    <a href="javascript:;" class="text-primary" data-bs-toggle="tooltip"
+                                        data-bs-placement="bottom" title="" data-bs-original-title="View detail"
+                                        aria-label="Views"><i class="bi bi-eye-fill"></i></a>
+                                    <a href="javascript:;" class="text-warning" data-bs-toggle="tooltip"
+                                        data-bs-placement="bottom" title="" data-bs-original-title="Edit info"
+                                        aria-label="Edit"><i class="bi bi-pencil-fill"></i></a>
+                                    <a href="javascript:;" class="text-danger" data-bs-toggle="tooltip"
+                                        data-bs-placement="bottom" title="" data-bs-original-title="Delete"
+                                        aria-label="Delete"><i class="bi bi-trash-fill"></i></a>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            <nav class="float-end mt-4" aria-label="Page navigation">
+                <ul class="pagination">
+                    <li class="page-item disabled"><a class="page-link" href="#">Previous</a></li>
+                    <li class="page-item active"><a class="page-link" href="#">1</a></li>
+                    <li class="page-item"><a class="page-link" href="#">2</a></li>
+                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                    <li class="page-item"><a class="page-link" href="#">Next</a></li>
+                </ul>
+            </nav>
+
+        </div>
+    </div>
 </div>
--- resources/views/livewire/admin/update-products-component.blade.php
+++ resources/views/livewire/admin/update-products-component.blade.php
@@ -1,3 +1,73 @@
 <div>
-    {{-- The best athlete wants his opponent at his best. --}}
+    <div class="row">
+        <div class="col-lg-8 mx-auto">
+         <div class="card">
+           <div class="card-header py-3 bg-transparent"> 
+              <h5 class="mb-0">Update Product</h5>
+             </div>
+           <div class="card-body">
+             <div class="border p-3 rounded">
+             <form class="row g-3">
+               <div class="col-12">
+                 <label class="form-label">Product title</label>
+                 <input type="text" class="form-control" placeholder="Product title">
+               </div>
+               <div class="col-12">
+                 <label class="form-label">Full description</label>
+                 <textarea class="form-control" placeholder="Full description" rows="4" cols="4"></textarea>
+               </div>
+               <div class="col-12">
+                 <label class="form-label">Images</label>
+                 <input class="form-control" type="file">
+               </div>
+               <div class="col-12 col-md-6">
+                 <label class="form-label">Category</label>
+                 <select class="form-select">
+                   <option>Select Category</option>
+                   <option>Fashion</option>
+                   <option>Electronics</option>
+                   <option>Furniture</option>
+                   <option>Sports</option>
+                 </select>
+               </div>
+               <div class="col-12 col-md-6">
+                 <label class="form-label">Status</label>
+                 <select class="form-select">
+                   <option>Select Status</option>
+                   <option>Normal</option>
+                   <option>Flash Sale</option>
+                 </select>
+               </div>
+               <div class="col-12">
+                <div class="row g-3">
+                  <div class="col-lg-4">
+                   <label class="form-label">Regular Price</label>
+                    <input type="text" class="form-control" placeholder="Price">
+                  </div>
+                  <div class="col-lg-4">
+                   <label class="form-label">Discounted Price</label>
+                   <input type="text" class="form-control" placeholder="Price">
+                 </div>
+                  <div class="col-lg-4">
+                   <label class="form-label">Subscription</label>
+                    <div class="input-group">
+                      <select class="form-select">
+                        <option>Select Subscription</option>
+                        <option>No Subscription</option>
+                        <option>1 Month</option>
+                        <option>2 Month</option>
+                      </select>
+                    </div>
+                  </div>
+                </div>
+              </div>
+               <div class="col-12">
+                 <button class="btn btn-primary px-4">Update Item</button>
+               </div>
+             </form>
+             </div>
+            </div>
+           </div>
+        </div>
+     </div><!--end row-->
 </div>
