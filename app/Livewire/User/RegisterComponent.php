eb7650d9d1723f1c1e7646cb12f32e3b076b0fec
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-02 15:28:08 +0600

    register code added

--- app/Livewire/User/RegisterComponent.php
+++ app/Livewire/User/RegisterComponent.php
@@ -2,10 +2,47 @@
 
 namespace App\Livewire\User;
 
+use App\Models\User;
 use Livewire\Component;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterComponent extends Component
 {
+    public $full_name, $email, $phone, $password, $confirm_password;
+    public function register()
+    {
+        $validatedData = $this->validate([
+            'full_name' => 'required|min:6',
+            'email' => 'required|email|unique:users',
+            'phone' => 'required|numeric',
+            'password' => 'required|min:6|same:confirm_password',
+            'confirm_password' => 'required|min:6',
+        ]);
+
+        if (!$validatedData) {
+            foreach ($validatedData as $key => $value) {
+                if ($value) {
+                    $errors[$key] = $value;
+                }
+            }
+            foreach ($errors as $key => $value) {
+                $this->addError($key, $value);
+            }
+        }
+
+        $user = new User();
+        $user->name = $this->full_name;
+        $user->email = $this->email;
+        $user->phone_number = $this->phone;
+        $user->password = Hash::make($this->password);
+        if ($user->save()) {
+            session()->flash('success', 'Registration is successfully.');
+        } else {
+            session()->flash('error', 'Something went wrong! Please try again.');
+        }
+
+        return redirect()->route('login')->with('success', 'Registration was successfull, Please login to your account.');
+    }
     public function render()
     {
         return view('livewire.user.register-component');
--- resources/views/livewire/user/register-component.blade.php
+++ resources/views/livewire/user/register-component.blade.php
@@ -1,45 +1,71 @@
 <div>
-        <section class="login account footer-padding">
-            <div class="container">
-                <div class="login-section account-section">
+    <section class="login account footer-padding">
+        <div class="container">
+            @include('livewire.user.partials._alerts')
+            <div class="login-section account-section">
+                <form wire:submit.prevent="register">
                     <div class="review-form">
                         <h5 class="comment-title">Create Account</h5>
                         <div class=" account-inner-form">
                             <div class="review-form-name">
                                 <label for="full_name" class="form-label">Full Name*</label>
-                                <input type="text" id="full_name" class="form-control" placeholder="First Name">
+                                <input type="text" id="full_name"
+                                    class="form-control @error('full_name') is-invalid @enderror" placeholder="Full Name" wire:model="full_name">
+                                @error('full_name')
+                                    <span class="invalid-feedback"> {{ $message }}</span>
+                                @enderror
                             </div>
                         </div>
                         <div class="account-inner-form">
                             <div class="review-form-name">
                                 <label for="email" class="form-label">Email*</label>
-                                <input type="email" id="email" class="form-control" placeholder="user@gmail.com">
+                                <input type="email" id="email"
+                                    class="form-control @error('email') is-invalid @enderror"
+                                    placeholder="user@gmail.com" wire:model="email">
+                                @error('email')
+                                    <span class="invalid-feedback"> {{ $message }}</span>
+                                @enderror
                             </div>
                         </div>
                         <div class="account-inner-form">
                             <div class="review-form-name">
                                 <label for="phone" class="form-label">Phone*</label>
-                                <input type="tel" id="phone" class="form-control" placeholder="+04XXXXXXXXXX">
+                                <input type="tel" id="phone"
+                                    class="form-control @error('phone') is-invalid @enderror"
+                                    placeholder="+04XXXXXXXXXX" wire:model="phone">
+                                @error('phone')
+                                    <span class="invalid-feedback"> {{ $message }}</span>
+                                @enderror
                             </div>
                         </div>
                         <div class="account-inner-form">
                             <div class="review-form-name">
                                 <label for="password" class="form-label">Password*</label>
-                                <input type="password" id="password" class="form-control" placeholder="Password">
+                                <input type="password" id="password"
+                                    class="form-control @error('password') is-invalid @enderror" placeholder="Password" wire:model="password">
+                                @error('password')
+                                    <span class="invalid-feedback"> {{ $message }}</span>
+                                @enderror
                             </div>
                         </div>
                         <div class="account-inner-form">
                             <div class="review-form-name">
                                 <label for="confirm_password" class="form-label">Confirm Password*</label>
-                                <input type="password" id="confirm_password" class="form-control" placeholder="Enter Same Password">
+                                <input type="password" id="confirm_password"
+                                    class="form-control @error('confirm_password') is-invalid @enderror"
+                                    placeholder="Enter Same Password" wire:model="confirm_password">
+                                @error('confirm_password')
+                                    <span class="invalid-feedback"> {{ $message }}</span>
+                                @enderror
                             </div>
                         </div>
                         <div class="login-btn text-center">
-                            <a href="#" class="shop-btn">Create an Account</a>
+                            <button type="submit" class="shop-btn">Create an Account</button>
                             <span class="shop-account">Already have an account ?<a href="">Log In</a></span>
                         </div>
                     </div>
-                </div>
+                </form>
             </div>
-        </section>
+        </div>
+    </section>
 </div>
