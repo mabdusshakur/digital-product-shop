483dfc475e11c4c7bfbc2ccd35220496ab58f3e9
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-02 15:31:42 +0600

    Login activated

--- app/Livewire/User/LoginComponent.php
+++ app/Livewire/User/LoginComponent.php
@@ -6,6 +6,33 @@ use Livewire\Component;
 
 class LoginComponent extends Component
 {
+    public $email, $password;
+
+    public function login()
+    {
+        $validatedData = $this->validate([
+            'email' => 'required|email',
+            'password' => 'required|min:6',
+        ]);
+
+        if (!$validatedData) {
+            foreach ($validatedData as $key => $value) {
+                if ($value) {
+                    $errors[$key] = $value;
+                }
+            }
+            foreach ($errors as $key => $value) {
+                $this->addError($key, $value);
+            }
+        }
+
+        if (auth()->attempt(['email' => $this->email, 'password' => $this->password])) {
+            return redirect()->route('user.home');
+        } else {
+            session()->flash('error', 'Invalid email or password.');
+            return redirect()->route('login');
+        }
+    }
     public function render()
     {
         return view('livewire.user.login-component');
--- resources/views/livewire/user/login-component.blade.php
+++ resources/views/livewire/user/login-component.blade.php
@@ -1,26 +1,39 @@
 <div>
     <section class="login account footer-padding">
         <div class="container">
+            @include('livewire.user.partials._alerts')
             <div class="login-section account-section">
-                <div class="review-form">
-                    <h5 class="comment-title">Login</h5>
-                    <div class="account-inner-form">
-                        <div class="review-form-name">
-                            <label for="email" class="form-label">Email*</label>
-                            <input type="email" id="email" class="form-control" placeholder="user@gmail.com">
+                <form wire:submit.prevent="login">
+                    <div class="review-form">
+                        <h5 class="comment-title">Login</h5>
+                        <div class="account-inner-form">
+                            <div class="review-form-name">
+                                <label for="email" class="form-label">Email*</label>
+                                <input type="email" id="email"
+                                    class="form-control @error('email') is-invalid @enderror"
+                                    placeholder="user@gmail.com" wire:model="email">
+                                @error('email')
+                                    <span class="invalid-feedback"> {{ $message }}</span>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="account-inner-form">
+                            <div class="review-form-name">
+                                <label for="password" class="form-label">Password*</label>
+                                <input type="password" id="password"
+                                    class="form-control @error('password') is-invalid @enderror" placeholder="Password"
+                                    wire:model="password">
+                                @error('password')
+                                    <span class="invalid-feedback"> {{ $message }}</span>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="login-btn text-center">
+                            <button type="submit" class="shop-btn">Login</a>
+                                <span class="shop-account">New User ?<a href="">Register</a></span>
                         </div>
                     </div>
-                    <div class="account-inner-form">
-                        <div class="review-form-name">
-                            <label for="password" class="form-label">Password*</label>
-                            <input type="password" id="password" class="form-control" placeholder="Password">
-                        </div>
-                    </div>
-                    <div class="login-btn text-center">
-                        <a href="#" class="shop-btn">Login</a>
-                        <span class="shop-account">New User ?<a href="">Register</a></span>
-                    </div>
-                </div>
+                </form>
             </div>
         </div>
     </section>
