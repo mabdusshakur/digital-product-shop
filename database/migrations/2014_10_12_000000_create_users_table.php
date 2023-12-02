df43b71e18b9c210e8fc8b114463b50803396149
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-02 15:01:36 +0600

    role field added on user  table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('phone_number')->unique();
+            $table->string('role')->default('user');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->timestamps();
