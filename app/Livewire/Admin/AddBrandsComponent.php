5cf41cc684a577d29beae5331008a3c95c8b6378
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-05 11:08:20 +0600

    add brand from added

--- app/Livewire/Admin/AddBrandsComponent.php
+++ app/Livewire/Admin/AddBrandsComponent.php
@@ -2,10 +2,47 @@
 
 namespace App\Livewire\Admin;
 
+use App\Models\Brand;
 use Livewire\Component;
+use Illuminate\Support\Str;
+use Livewire\WithFileUploads;
 
 class AddBrandsComponent extends Component
 {
+    use WithFileUploads;
+
+    public $name, $logo;
+
+    public function add_category()
+    {
+        $validatedData = $this->validate([
+            'name' => 'required',
+            'logo' => 'required|mimes:jpeg,png',
+        ]);
+
+        if (!$validatedData) {
+            foreach ($validatedData as $key => $value) {
+                if ($value) {
+                    $errors[$key] = $value;
+                }
+            }
+            foreach ($errors as $key => $value) {
+                $this->addError($key, $value);
+            }
+        }
+
+        $category = new Brand();
+        $category->name = $this->name;
+        $category->slug = Str::slug($this->name);
+        $imageName = time().'.'.$this->logo->getClientOriginalExtension();
+        $imageLocation = $this->logo->storeAs('brands', $imageName, 'public');
+        $category->image = $imageLocation;
+        if ($category->save()) {
+            session()->flash('success', 'Brand has been created successfully!');
+        } else {
+            session()->flash('error', 'Something went wrong!');
+        }
+    }
     public function render()
     {
         return view('livewire.admin.add-brands-component')->layout('components.layouts.admin');
--- resources/views/livewire/admin/add-brands-component.blade.php
+++ resources/views/livewire/admin/add-brands-component.blade.php
@@ -1,3 +1,37 @@
 <div>
-    {{-- A good traveler has no fixed plans and is not intent upon arriving. --}}
+    <div class="row">
+        <div class="col-lg-8 mx-auto">
+            <div class="card">
+                @include('livewire.admin.partials._alerts')
+                <div class="card-header py-3 bg-transparent">
+                    <h5 class="mb-0">Add Brands</h5>
+                </div>
+                <div class="card-body">
+                    <div class="border p-3 rounded">
+                        <form wire:submit.prevent="add_category" class="row g-3">
+                            <div class="col-12">
+                                <label class="form-label">Brand Name</label>
+                                <input type="text" class="form-control @error('name') is-invalid @enderror"
+                                    placeholder="Category Name" wire:model="name">
+                                @error('name')
+                                    <span class="invalid-feedback"> {{ $message }}</span>
+                                @enderror
+                            </div>
+                            <div class="col-12">
+                                <label class="form-label">Logo</label>
+                                <input class="form-control @error('logo') is-invalid @enderror" type="file"
+                                    wire:model="logo">
+                                @error('logo')
+                                    <span class="invalid-feedback"> {{ $message }}</span>
+                                @enderror
+                            </div>
+                            <div class="col-12">
+                                <button class="btn btn-primary px-4">Submit Brand</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
