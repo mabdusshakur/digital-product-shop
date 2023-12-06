88ea669be46e1f44e3ea417d2de05521602d5f11
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-06 11:30:30 +0600

    save phone number on save settings component added

--- resources/views/livewire/admin/settings-component.blade.php
+++ resources/views/livewire/admin/settings-component.blade.php
@@ -1,3 +1,29 @@
 <div>
-    {{-- Stop trying to control. --}}
+    <div class="row">
+        <div class="col-lg-8 mx-auto">
+            <div class="card">
+                @include('livewire.admin.partials._alerts')
+                <div class="card-header py-3 bg-transparent">
+                    <h5 class="mb-0">Settings</h5>
+                </div>
+                <div class="card-body">
+                    <div class="border p-3 rounded">
+                        <form wire:submit.prevent="save_settings" class="row g-3">
+                            <div class="col-12">
+                                <label class="form-label">Phone Number</label>
+                                <input type="text" class="form-control @error('phone_number') is-invalid @enderror"
+                                    placeholder="+0000000000" wire:model="phone_number">
+                                @error('phone_number')
+                                    <span class="invalid-feedback"> {{ $message }}</span>
+                                @enderror
+                            </div>
+                            <div class="col-12">
+                                <button class="btn btn-primary px-4">Save Setting</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
