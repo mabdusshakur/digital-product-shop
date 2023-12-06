278a505ae35c72bc9666d68ba0eeab01d5f08fac
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-06 11:38:20 +0600

    simple setting migration added

---
+++ database/migrations/2023_12_06_053537_create_settings_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->id();
+            $table->string('phone_number');
+            $table->string('address');
+            $table->string('web_name');
+            $table->string('logo');
+            $table->string('favicon');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('settings');
+    }
+};
