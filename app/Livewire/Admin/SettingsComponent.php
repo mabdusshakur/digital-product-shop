5ad1aa87a54da26a0952db97209e1cd35596ec6b
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-06 11:35:22 +0600

    save setting function added

--- app/Livewire/Admin/SettingsComponent.php
+++ app/Livewire/Admin/SettingsComponent.php
@@ -6,6 +6,24 @@ use Livewire\Component;
 
 class SettingsComponent extends Component
 {
+    public $phone_number;
+    public function save_settings()
+    {
+        $validatedData = $this->validate([
+            'phone_number' => 'required|numeric',
+        ]);
+
+        if (!$validatedData) {
+            foreach ($validatedData as $key => $value) {
+                if ($value) {
+                    $errors[$key] = $value;
+                }
+            }
+            foreach ($errors as $key => $value) {
+                $this->addError($key, $value);
+            }
+        }
+    }
     public function render()
     {
         return view('livewire.admin.settings-component')->layout('components.layouts.admin');
