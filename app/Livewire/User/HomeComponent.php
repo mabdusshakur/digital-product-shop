b264b9dcaab89a224ebc4941691b92327dc39055
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-07 22:56:42 +0600

    categories and product mounted for homeComponent

--- app/Livewire/User/HomeComponent.php
+++ app/Livewire/User/HomeComponent.php
@@ -2,10 +2,18 @@
 
 namespace App\Livewire\User;
 
+use App\Models\Product;
 use Livewire\Component;
+use App\Models\Category;
 
 class HomeComponent extends Component
 {
+    public $categories, $products;
+    public function mount()
+    {
+        $this->categories = Category::all();
+        $this->products = Product::all();
+    }
     public function render()
     {
         return view('livewire.user.home-component');
