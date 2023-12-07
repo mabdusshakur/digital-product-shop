1d2fff628693aa41751792c720a4542c634ba8fe
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-07 16:42:52 +0600

    footer setting optional added

--- app/Livewire/User/FooterComponent.php
+++ app/Livewire/User/FooterComponent.php
@@ -3,6 +3,7 @@
 namespace App\Livewire\User;
 
 use App\Models\Setting;
+use Illuminate\Support\Optional;
 use Livewire\Component;
 
 class FooterComponent extends Component
@@ -10,7 +11,7 @@ class FooterComponent extends Component
     public $phone_number;
     public function mount()
     {
-       $this->phone_number = Setting::find(1)->phone_number;
+       $this->phone_number = Optional(Setting::find(1))->phone_number;
     }
     
     public function render()
