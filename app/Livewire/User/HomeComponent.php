9005aedb47032d41f58786d0c8a8fbe3c6fe07fa
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-07 22:59:19 +0600

    brands mounted for homeComponent

--- app/Livewire/User/HomeComponent.php
+++ app/Livewire/User/HomeComponent.php
@@ -2,17 +2,19 @@
 
 namespace App\Livewire\User;
 
+use App\Models\Brand;
 use App\Models\Product;
 use Livewire\Component;
 use App\Models\Category;
 
 class HomeComponent extends Component
 {
-    public $categories, $products;
+    public $categories, $products, $brands;
     public function mount()
     {
         $this->categories = Category::all();
         $this->products = Product::all();
+        $this->brands = Brand::all();
     }
     public function render()
     {
