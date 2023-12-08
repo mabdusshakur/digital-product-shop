378b707f2c2164ad8e82902ef7efa22d309465ab
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-08 13:03:02 +0600

    mount added for the parameters

--- app/Livewire/User/ProductDetailsComponent.php
+++ app/Livewire/User/ProductDetailsComponent.php
@@ -6,6 +6,10 @@ use Livewire\Component;
 
 class ProductDetailsComponent extends Component
 {
+    public function mount($id, $slug = null, $category_id = null)
+    {
+        dd($id, $slug, $category_id);
+    }
     public function render()
     {
         return view('livewire.user.product-details-component');
