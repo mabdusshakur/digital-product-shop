bc59b4bdd094e84c3f76104472bacf30dd70d6ed
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-08 14:38:02 +0600

    delete all and single wishlist item added

--- app/Livewire/User/WishListComponent.php
+++ app/Livewire/User/WishListComponent.php
@@ -7,6 +7,23 @@ use Livewire\Component;
 
 class WishListComponent extends Component
 {
+    public function removeWishlist($id)
+    {
+        $wishlist = Wishlist::find($id);
+        if($wishlist->delete())
+            session()->flash('success', 'Product has been removed from wishlist successfully!');
+        else
+            session()->flash('error', 'Something went wrong!');
+    }
+    public function clearAllWishlist()
+    {
+        $wishlists = Wishlist::where('user_id', auth()->user()->id)->get();
+        foreach($wishlists as $wishlist)
+        {
+            $wishlist->delete();
+        }
+        session()->flash('success', 'Wishlist has been cleared successfully!');
+    }
     public function render()
     {
         $wishlists = Wishlist::where('user_id', auth()->user()->id)->get();
--- resources/views/livewire/user/wish-list-component.blade.php
+++ resources/views/livewire/user/wish-list-component.blade.php
@@ -1,5 +1,6 @@
 <div>
     <section class="cart product wishlist footer-padding" data-aos="fade-up">
+        @include('livewire.user.partials._alerts')
         <div class="container">
             <div class="cart-section wishlist-section">
                 <table>
@@ -21,7 +22,8 @@
                                     <td class="table-wrapper wrapper-product">
                                         <div class="wrapper">
                                             <div class="wrapper-img">
-                                                <img src="{{ Storage::url($wishlist->product->image[0]->image) }}" alt="{{ $wishlist->product->name }}">
+                                                <img src="{{ Storage::url($wishlist->product->image[0]->image) }}"
+                                                    alt="{{ $wishlist->product->name }}">
                                             </div>
                                             <div class="wrapper-content">
                                                 <h5 class="heading">{{ $wishlist->product->name }}</h5>
@@ -30,14 +32,16 @@
                                     </td>
                                     <td class="table-wrapper">
                                         <div class="table-wrapper-center">
-                                            <span>
-                                                <svg width="10" height="10" viewBox="0 0 10 10" fill="none"
-                                                    xmlns="http://www.w3.org/2000/svg">
-                                                    <path
-                                                        d="M9.7 0.3C9.3 -0.1 8.7 -0.1 8.3 0.3L5 3.6L1.7 0.3C1.3 -0.1 0.7 -0.1 0.3 0.3C-0.1 0.7 -0.1 1.3 0.3 1.7L3.6 5L0.3 8.3C-0.1 8.7 -0.1 9.3 0.3 9.7C0.7 10.1 1.3 10.1 1.7 9.7L5 6.4L8.3 9.7C8.7 10.1 9.3 10.1 9.7 9.7C10.1 9.3 10.1 8.7 9.7 8.3L6.4 5L9.7 1.7C10.1 1.3 10.1 0.7 9.7 0.3Z"
-                                                        fill="#AAAAAA"></path>
-                                                </svg>
-                                            </span>
+                                            <a href="javascript:;" wire:click="removeWishlist({{$wishlist->id}})">
+                                                <span>
+                                                    <svg width="10" height="10" viewBox="0 0 10 10"
+                                                        fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                        <path
+                                                            d="M9.7 0.3C9.3 -0.1 8.7 -0.1 8.3 0.3L5 3.6L1.7 0.3C1.3 -0.1 0.7 -0.1 0.3 0.3C-0.1 0.7 -0.1 1.3 0.3 1.7L3.6 5L0.3 8.3C-0.1 8.7 -0.1 9.3 0.3 9.7C0.7 10.1 1.3 10.1 1.7 9.7L5 6.4L8.3 9.7C8.7 10.1 9.3 10.1 9.7 9.7C10.1 9.3 10.1 8.7 9.7 8.3L6.4 5L9.7 1.7C10.1 1.3 10.1 0.7 9.7 0.3Z"
+                                                            fill="#AAAAAA"></path>
+                                                    </svg>
+                                                </span>
+                                            </a>
                                         </div>
                                     </td>
                                 </tr>
@@ -48,7 +52,7 @@
                 </table>
             </div>
             <div class="wishlist-btn">
-                <a href="empty-wishlist.html" class="clean-btn">Clean Wishlist</a>
+                <a href="" wire:click="clearAllWishlist" class="clean-btn">Clean Wishlist</a>
                 <a href="#" class="shop-btn">View Cards</a>
             </div>
         </div>
