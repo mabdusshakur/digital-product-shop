ffb9f0b7608049639ac02b5079555f37d8c56613
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-09 13:41:09 +0600

    image added

--- app/Models/About.php
+++ app/Models/About.php
@@ -8,5 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class About extends Model
 {
     use HasFactory;
-    protected $fillable = ['title', 'description'];
+    protected $fillable = ['title', 'description', 'image'];
 }
--- database/migrations/2023_11_30_135623_create_abouts_table.php
+++ database/migrations/2023_11_30_135623_create_abouts_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->longText('description');
+            $table->string('image');
             $table->timestamps();
         });
     }
