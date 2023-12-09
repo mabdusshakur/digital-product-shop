f1b80c095abad745d32c3399ff3c30d37b41fba7
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-09 14:03:34 +0600

    dynamic about page added

--- app/Livewire/User/AboutUsComponent.php
+++ app/Livewire/User/AboutUsComponent.php
@@ -2,10 +2,16 @@
 
 namespace App\Livewire\User;
 
+use App\Models\About;
 use Livewire\Component;
 
 class AboutUsComponent extends Component
 {
+    public $about;
+    public function mount()
+    {
+        $this->about = About::find(1);
+    }
     public function render()
     {
         return view('livewire.user.about-us-component');
