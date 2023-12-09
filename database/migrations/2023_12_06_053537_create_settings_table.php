1f8eae0be95e6aa8355b96c118edf71fc22a59e7
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-09 14:18:24 +0600

    longText added from string

--- database/migrations/2023_12_06_053537_create_settings_table.php
+++ database/migrations/2023_12_06_053537_create_settings_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('logo')->nullable();
             $table->string('favicon')->nullable();
             $table->string('email')->nullable();
-            $table->string('iframe_map_link')->nullable();
+            $table->longText('iframe_map_link')->nullable();
             $table->timestamps();
         });
     }
