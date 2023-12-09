be77f4c7a872cd800ab8085c6675793d56ad53c9
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-09 13:34:36 +0600

    products with subtotal added

--- app/Livewire/User/MiniCartComponent.php
+++ app/Livewire/User/MiniCartComponent.php
@@ -2,12 +2,16 @@
 
 namespace App\Livewire\User;
 
+use App\Models\Cart;
 use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
 
 class MiniCartComponent extends Component
 {
+    public $subtotal;
     public function render()
     {
-        return view('livewire.user.mini-cart-component');
+        $cartItems = Cart::where('user_id', Auth::user()->id)->get();
+        return view('livewire.user.mini-cart-component', ['cartItems' => $cartItems]);
     }
 }
