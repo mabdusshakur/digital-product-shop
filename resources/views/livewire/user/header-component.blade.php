5539d16edeb7bd1e96af288f6cc2157ac668ddcf
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-09 13:25:04 +0600

    mini cart seperated

--- resources/views/livewire/user/header-component.blade.php
+++ resources/views/livewire/user/header-component.blade.php
@@ -110,47 +110,7 @@
                                     Cart
                                 </span>
                             </a>
-                            <div class="cart-submenu">
-                                <div class="cart-wrapper-item">
-
-
-                                    <div class="wrapper">
-                                        <div class="wrapper-item">
-                                            <div class="wrapper-img">
-                                                <img src=""
-                                                    alt="img">
-                                            </div>
-                                            <div class="wrapper-content">
-                                                <h5 class="wrapper-title">Netflix 1 Year</h5>
-                                                <div class="price">
-                                                    <p class="new-price">$20.00</p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <span class="close-btn">
-                                            <svg viewBox="0 0 10 10" fill="none" class="fill-current"
-                                                xmlns="http://www.w3.org/2000/svg">
-                                                <path
-                                                    d="M9.7 0.3C9.3 -0.1 8.7 -0.1 8.3 0.3L5 3.6L1.7 0.3C1.3 -0.1 0.7 -0.1 0.3 0.3C-0.1 0.7 -0.1 1.3 0.3 1.7L3.6 5L0.3 8.3C-0.1 8.7 -0.1 9.3 0.3 9.7C0.7 10.1 1.3 10.1 1.7 9.7L5 6.4L8.3 9.7C8.7 10.1 9.3 10.1 9.7 9.7C10.1 9.3 10.1 8.7 9.7 8.3L6.4 5L9.7 1.7C10.1 1.3 10.1 0.7 9.7 0.3Z">
-                                                </path>
-                                            </svg>
-                                        </span>
-                                    </div>
-
-                                    
-                                </div>
-                                <div class="cart-wrapper-section">
-                                    <div class="wrapper-line"></div>
-                                    <div class="wrapper-subtotal">
-                                        <h5 class="wrapper-title">Subtotal</h5>
-                                        <h5 class="wrapper-title">$55</h5>
-                                    </div>
-                                    <div class="cart-btn">
-                                        <a href="cart.html" class="shop-btn view-btn">View Cart</a>
-                                        <a href="checkout.html" class="shop-btn checkout-btn">Checkout Now</a>
-                                    </div>
-                                </div>
-                            </div>
+                            @livewire('user.mini-cart-component')
                         </div>
                         <div class="header-user">
                             <a href="{{route('user.profile')}}">
