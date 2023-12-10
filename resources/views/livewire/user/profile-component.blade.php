12da1a4edcf39398f16760f2f534f4bf47e57484
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-10 11:06:08 +0600

    unnecessary button fields removed

--- resources/views/livewire/user/profile-component.blade.php
+++ resources/views/livewire/user/profile-component.blade.php
@@ -477,6 +477,8 @@
     
                                 <div class="top-selling-section">
                                     <div class="row g-5">
+
+
                                         <div class="col-md-6">
                                             <div class="product-wrapper">
                                                 <div class="product-img">
@@ -522,141 +524,8 @@
                                                 </div>
                                             </div>
                                         </div>
-                                        <div class="col-md-6">
-                                            <div class="product-wrapper">
-                                                <div class="product-img">
-                                                    <img src="./assets/images/homepage-one/product-img/product-img-6.webp"
-                                                        alt="product-img">
-                                                </div>
-                                                <div class="product-info">
-                                                    <div class="review-date">
-                                                        <p>July 22, 2022</p>
-                                                    </div>
-                                                    <div class="ratings">
-                                                        <span>
-                                                            <svg width="75" height="15" viewBox="0 0 75 15" fill="none"
-                                                                xmlns="http://www.w3.org/2000/svg">
-                                                                <path
-                                                                    d="M7.5 0L9.18386 5.18237H14.6329L10.2245 8.38525L11.9084 13.5676L7.5 10.3647L3.09161 13.5676L4.77547 8.38525L0.367076 5.18237H5.81614L7.5 0Z"
-                                                                    fill="#FFA800" />
-                                                                <path
-                                                                    d="M22.5 0L24.1839 5.18237H29.6329L25.2245 8.38525L26.9084 13.5676L22.5 10.3647L18.0916 13.5676L19.7755 8.38525L15.3671 5.18237H20.8161L22.5 0Z"
-                                                                    fill="#FFA800" />
-                                                                <path
-                                                                    d="M37.5 0L39.1839 5.18237H44.6329L40.2245 8.38525L41.9084 13.5676L37.5 10.3647L33.0916 13.5676L34.7755 8.38525L30.3671 5.18237H35.8161L37.5 0Z"
-                                                                    fill="#FFA800" />
-                                                                <path
-                                                                    d="M52.5 0L54.1839 5.18237H59.6329L55.2245 8.38525L56.9084 13.5676L52.5 10.3647L48.0916 13.5676L49.7755 8.38525L45.3671 5.18237H50.8161L52.5 0Z"
-                                                                    fill="#FFA800" />
-                                                                <path
-                                                                    d="M67.5 0L69.1839 5.18237H74.6329L70.2245 8.38525L71.9084 13.5676L67.5 10.3647L63.0916 13.5676L64.7755 8.38525L60.3671 5.18237H65.8161L67.5 0Z"
-                                                                    fill="#FFA800" />
-                                                            </svg>
-                                                        </span>
-                                                    </div>
-                                                    <div class="product-description">
-                                                        <a href="product-sidebar.html" class="product-details">Rainbow
-                                                            Sequin Dress
-                                                        </a>
-                                                        <p>Didn't I tell you not put your phone on charge because weekend?
-                                                        </p>
-                                                    </div>
-                                                </div>
-                                                <div class="product-cart-btn">
-                                                    <a href="cart.html" class="product-btn">Edit Review</a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="col-md-6">
-                                            <div class="product-wrapper">
-                                                <div class="product-img">
-                                                    <img src="./assets/images/homepage-one/product-img/product-img-7.webp"
-                                                        alt="product-img">
-                                                </div>
-                                                <div class="product-info">
-                                                    <div class="review-date">
-                                                        <p>July 22, 2022</p>
-                                                    </div>
-                                                    <div class="ratings">
-                                                        <span>
-                                                            <svg width="75" height="15" viewBox="0 0 75 15" fill="none"
-                                                                xmlns="http://www.w3.org/2000/svg">
-                                                                <path
-                                                                    d="M7.5 0L9.18386 5.18237H14.6329L10.2245 8.38525L11.9084 13.5676L7.5 10.3647L3.09161 13.5676L4.77547 8.38525L0.367076 5.18237H5.81614L7.5 0Z"
-                                                                    fill="#FFA800" />
-                                                                <path
-                                                                    d="M22.5 0L24.1839 5.18237H29.6329L25.2245 8.38525L26.9084 13.5676L22.5 10.3647L18.0916 13.5676L19.7755 8.38525L15.3671 5.18237H20.8161L22.5 0Z"
-                                                                    fill="#FFA800" />
-                                                                <path
-                                                                    d="M37.5 0L39.1839 5.18237H44.6329L40.2245 8.38525L41.9084 13.5676L37.5 10.3647L33.0916 13.5676L34.7755 8.38525L30.3671 5.18237H35.8161L37.5 0Z"
-                                                                    fill="#FFA800" />
-                                                                <path
-                                                                    d="M52.5 0L54.1839 5.18237H59.6329L55.2245 8.38525L56.9084 13.5676L52.5 10.3647L48.0916 13.5676L49.7755 8.38525L45.3671 5.18237H50.8161L52.5 0Z"
-                                                                    fill="#FFA800" />
-                                                                <path
-                                                                    d="M67.5 0L69.1839 5.18237H74.6329L70.2245 8.38525L71.9084 13.5676L67.5 10.3647L63.0916 13.5676L64.7755 8.38525L60.3671 5.18237H65.8161L67.5 0Z"
-                                                                    fill="#FFA800" />
-                                                            </svg>
-                                                        </span>
-                                                    </div>
-                                                    <div class="product-description">
-                                                        <a href="product-sidebar.html" class="product-details">Rainbow
-                                                            Sequin Dress
-                                                        </a>
-                                                        <p>Didn't I tell you not put your phone on charge because weekend?
-                                                        </p>
-                                                    </div>
-                                                </div>
-                                                <div class="product-cart-btn">
-                                                    <a href="cart.html" class="product-btn">Edit Review</a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div class="col-md-6">
-                                            <div class="product-wrapper">
-                                                <div class="product-img">
-                                                    <img src="./assets/images/homepage-one/product-img/product-img-8.webp"
-                                                        alt="product-img">
-                                                </div>
-                                                <div class="product-info">
-                                                    <div class="review-date">
-                                                        <p>July 22, 2022</p>
-                                                    </div>
-                                                    <div class="ratings">
-                                                        <span>
-                                                            <svg width="75" height="15" viewBox="0 0 75 15" fill="none"
-                                                                xmlns="http://www.w3.org/2000/svg">
-                                                                <path
-                                                                    d="M7.5 0L9.18386 5.18237H14.6329L10.2245 8.38525L11.9084 13.5676L7.5 10.3647L3.09161 13.5676L4.77547 8.38525L0.367076 5.18237H5.81614L7.5 0Z"
-                                                                    fill="#FFA800" />
-                                                                <path
-                                                                    d="M22.5 0L24.1839 5.18237H29.6329L25.2245 8.38525L26.9084 13.5676L22.5 10.3647L18.0916 13.5676L19.7755 8.38525L15.3671 5.18237H20.8161L22.5 0Z"
-                                                                    fill="#FFA800" />
-                                                                <path
-                                                                    d="M37.5 0L39.1839 5.18237H44.6329L40.2245 8.38525L41.9084 13.5676L37.5 10.3647L33.0916 13.5676L34.7755 8.38525L30.3671 5.18237H35.8161L37.5 0Z"
-                                                                    fill="#FFA800" />
-                                                                <path
-                                                                    d="M52.5 0L54.1839 5.18237H59.6329L55.2245 8.38525L56.9084 13.5676L52.5 10.3647L48.0916 13.5676L49.7755 8.38525L45.3671 5.18237H50.8161L52.5 0Z"
-                                                                    fill="#FFA800" />
-                                                                <path
-                                                                    d="M67.5 0L69.1839 5.18237H74.6329L70.2245 8.38525L71.9084 13.5676L67.5 10.3647L63.0916 13.5676L64.7755 8.38525L60.3671 5.18237H65.8161L67.5 0Z"
-                                                                    fill="#FFA800" />
-                                                            </svg>
-                                                        </span>
-                                                    </div>
-                                                    <div class="product-description">
-                                                        <a href="product-sidebar.html" class="product-details">Rainbow
-                                                            Sequin Dress
-                                                        </a>
-                                                        <p>Didn't I tell you not put your phone on charge because weekend?
-                                                        </p>
-                                                    </div>
-                                                </div>
-                                                <div class="product-cart-btn">
-                                                    <a href="cart.html" class="product-btn">Edit Review</a>
-                                                </div>
-                                            </div>
-                                        </div>
+
+
                                     </div>
                                 </div>
                             </div>
@@ -685,13 +554,7 @@
                                             </form>
                                             <div class="form-btn">
                                                 <a href="#" class="shop-btn">Upldate Password</a>
-                                                <a href="#" class="shop-btn cancel-btn">Cancel</a>
                                             </div>
-                                        </div>
-                                    </div>
-                                    <div class="col-lg-6">
-                                        <div class="reset-img text-end">
-                                            <img src="./assets/images/homepage-one/reset.webp" alt="reset">
                                         </div>
                                     </div>
                                 </div>
