3f19dd3451d1591fd2650b6baafb798b1ec6339b
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-10 15:01:34 +0600

    different order items table added

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -5,8 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Order extends Model
+class OrderItem extends Model
 {
     use HasFactory;
-    protected $fillable=['status','price','quantity','product_id','user_id'];
 }
--- database/migrations/2023_12_10_090033_create_order_items_table.php
+++ database/migrations/2023_12_10_090033_create_order_items_table.php
@@ -11,11 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('orders', function (Blueprint $table) {
+        Schema::create('order_items', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->float('total_price');
-            $table->string('status')->default('pending');
+            $table->foreignId('order_id')->references('id')->on('orders')->onDelete('cascade');
+            $table->foreignId('product_id')->references('id')->on('products')->onDelete('cascade');
+            $table->integer('quantity');
+            $table->float('price');
             $table->timestamps();
         });
     }
@@ -25,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('orders');
+        Schema::dropIfExists('order_items');
     }
 };
