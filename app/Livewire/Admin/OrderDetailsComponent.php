509b4af7ea0609aaa375fda5002ae5cd4577f36d
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-13 05:28:58 +0600

    save details and deliver function added

--- app/Livewire/Admin/OrderDetailsComponent.php
+++ app/Livewire/Admin/OrderDetailsComponent.php
@@ -4,6 +4,8 @@ namespace App\Livewire\Admin;
 
 use App\Models\Order;
 use Livewire\Component;
+use App\Mail\DeliverProductMail;
+use Illuminate\Support\Facades\Mail;
 
 class OrderDetailsComponent extends Component
 {    
@@ -27,6 +29,40 @@ class OrderDetailsComponent extends Component
             $this->payment_number = $order->payment_number;
             $this->payment_transaction_id = $order->payment_transaction_id;
         }
+    }
+    public function save_order_details()
+    {
+        $order = Order::find($this->order_id);
+        if($order)
+        {
+            $order->update([
+                'status' => $this->status
+            ]);
+            session()->flash('success', 'Order status updated successfully.');
+        }
+    }
+
+    public function deliver()
+    {
+        $mail_data = [
+            'user_name' => $this->buyer_name,
+            'product' => $this->delivery_details,
+            'title' => $this->mail_title,
+            'order_id'=> $this->order_id
+        ];
+        if(Mail::to($this->buyer_email)->send(new DeliverProductMail($mail_data)))
+        {
+            Order::find($this->order_id)
+            ->update([
+                'status' => 'delivered'
+            ]);
+            session()->flash('success', 'Product delivered successfully.');
+        }
+        else
+        {
+            session()->flash('error', 'Something went wrong.');
+        }
+        
     }
     public function render()
     {
