2af545615b5e70bcc78705175b57bcc1f4e2d988
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-12 15:30:59 +0600

    delivery and return policy seperated

--- resources/views/livewire/user/delivery-returns-component.blade.php
+++ resources/views/livewire/user/delivery-returns-component.blade.php
@@ -3,16 +3,19 @@
         <div class="container">
             <div class="about-section">
                 <div class="row align-items-center gy-5">
-                    <div class="col-lg-6 mb-4">
-                        <div class="about-img" data-aos="fade-right">
-                            <img src="" alt="img">
+                    <div class="col-lg-6">
+                        <div class="about-content" data-aos="fade-up">
+                            <h3 class="about-title">Delivery Policy</h3>
+                            <p class="about-info">
+                                {{-- {!! $delivery_policy !!} --}}
+                            </p>
                         </div>
                     </div>
                     <div class="col-lg-6">
                         <div class="about-content" data-aos="fade-up">
-                            <h3 class="about-title">Delivery & Returns</h3>
+                            <h3 class="about-title">Return Policy</h3>
                             <p class="about-info">
-                                
+                                {{-- {!! $delivery_policy !!} --}}
                             </p>
                         </div>
                     </div>
