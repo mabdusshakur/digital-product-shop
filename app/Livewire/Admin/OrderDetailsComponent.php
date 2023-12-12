2081136fa1c967a1ba9f54ebd34fbf727955b0e8
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-13 05:28:22 +0600

    retrieved order details on the details page

--- app/Livewire/Admin/OrderDetailsComponent.php
+++ app/Livewire/Admin/OrderDetailsComponent.php
@@ -2,10 +2,32 @@
 
 namespace App\Livewire\Admin;
 
+use App\Models\Order;
 use Livewire\Component;
 
 class OrderDetailsComponent extends Component
-{
+{    
+    public $order_id, $buyer_name, $buyer_email, $buyer_phone, $status, $orderItems = [], $total_price, $payment_method, $payment_number, $payment_transaction_id;
+    public $delivery_details;
+    public $mail_title;
+
+    public function mount($id)
+    {
+        $order = Order::find($id);
+        if($order)
+        {
+            $this->order_id = $order->id;
+            $this->buyer_name = $order->user->name;
+            $this->buyer_email = $order->user->email;
+            $this->buyer_phone = $order->user->phone_number;
+            $this->status = $order->status;
+            $this->orderItems = $order->orderItems;
+            $this->total_price = $order->total_price;
+            $this->payment_method = $order->payment_method;
+            $this->payment_number = $order->payment_number;
+            $this->payment_transaction_id = $order->payment_transaction_id;
+        }
+    }
     public function render()
     {
         return view('livewire.admin.order-details-component');
