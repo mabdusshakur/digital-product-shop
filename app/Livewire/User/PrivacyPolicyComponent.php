4012f2d1ef1d910c92c21e2373b9707c4ae9077f
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-12 17:55:14 +0600

    dynamic policy added

--- app/Livewire/User/PrivacyPolicyComponent.php
+++ app/Livewire/User/PrivacyPolicyComponent.php
@@ -2,10 +2,18 @@
 
 namespace App\Livewire\User;
 
+use App\Models\Setting;
 use Livewire\Component;
 
 class PrivacyPolicyComponent extends Component
 {
+    public $privacy_policy;
+    public function mount()
+    {
+        $settings = Setting::get()->first();
+        $this->privacy_policy = Optional($settings)->privacy_policy;
+    }
+    
     public function render()
     {
         return view('livewire.user.privacy-policy-component');
