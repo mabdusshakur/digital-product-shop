b81d48cff517d09f576ee0652e315ee28ff09831
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-13 07:02:51 +0600

    old search code removed

--- app/Livewire/User/HeaderComponent.php
+++ app/Livewire/User/HeaderComponent.php
@@ -2,13 +2,11 @@
 
 namespace App\Livewire\User;
 
-use App\Models\Product;
 use App\Models\Setting;
 use Livewire\Component;
 
 class HeaderComponent extends Component
 {
-    public $search;
     public $phone_number;
     public function mount()
     {
@@ -17,15 +15,6 @@ class HeaderComponent extends Component
 
     public function render()
     {
-        if (strlen($this->search) > 2) {
-            $searchResults = Product::where('name', 'like', '%' . $this->search . '%')
-                ->orWhere('description', 'like', '%' . $this->search . '%')
-                ->get();
-        } else {
-            $searchResults = [];
-        }
-        return view('livewire.user.header-component', [
-            'searchResults' => $searchResults,
-        ]);
+        return view('livewire.user.header-component');
     }
 }
