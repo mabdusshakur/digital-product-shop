03c92751d35b080bec1e4e806999dd427d80b9cc
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-15 21:14:17 +0600

    search bar removed

--- resources/views/livewire/admin/header-component.blade.php
+++ resources/views/livewire/admin/header-component.blade.php
@@ -1,96 +1,93 @@
 <div>
-    <header class="top-header">        
+    <header class="top-header">
         <nav class="navbar navbar-expand gap-3 align-items-center">
-          <div class="mobile-toggle-icon fs-3">
-              <i class="bi bi-list"></i>
+            <div class="mobile-toggle-icon fs-3">
+                <i class="bi bi-list"></i>
             </div>
-            <form class="searchbar">
-                <div class="position-absolute top-50 translate-middle-y search-icon ms-3"><i class="bi bi-search"></i></div>
-                <input class="form-control" type="text" placeholder="Type here to search">
-                <div class="position-absolute top-50 translate-middle-y search-close-icon"><i class="bi bi-x-lg"></i></div>
-            </form>
-            <div class="top-navbar-right ms-auto">
-              <ul class="navbar-nav align-items-center">
-                <li class="nav-item search-toggle-icon">
-                  <a class="nav-link" href="#">
-                    <div class="">
-                      <i class="bi bi-search"></i>
-                    </div>
-                  </a>
-              </li>
 
-              <li class="nav-item dropdown dropdown-user-setting">
-                <a class="nav-link dropdown-toggle dropdown-toggle-nocaret" href="#" data-bs-toggle="dropdown">
-                  <div class="user-setting d-flex align-items-center">
-                    <img src="{{Storage::url(Auth::user()->profile_image)}}" class="user-img" alt="">
-                  </div>
-                </a>
-                <ul class="dropdown-menu dropdown-menu-end">
-                  <li>
-                     <a class="dropdown-item" href="#">
-                       <div class="d-flex align-items-center">
-                          <img src="" alt="" class="rounded-circle" width="54" height="54">
-                          <div class="ms-3">
-                            <h6 class="mb-0 dropdown-user-name">{{Auth::user()->name}}</h6>
-                            <small class="mb-0 dropdown-user-designation text-secondary">{{Auth::user()->role}}</small>
-                          </div>
-                       </div>
-                     </a>
-                   </li>
-                   <li><hr class="dropdown-divider"></li>
-                   <li>
-                      <a class="dropdown-item" href="pages-user-profile.html">
-                         <div class="d-flex align-items-center">
-                           <div class=""><i class="bi bi-person-fill"></i></div>
-                           <div class="ms-3"><span>Profile</span></div>
-                         </div>
-                       </a>
-                    </li>
-                    <li>
-                      <a class="dropdown-item" href="#">
-                         <div class="d-flex align-items-center">
-                           <div class=""><i class="bi bi-gear-fill"></i></div>
-                           <div class="ms-3"><span>Setting</span></div>
-                         </div>
-                       </a>
-                    </li>
-                    <li>
-                      <a class="dropdown-item" href="index2.html">
-                         <div class="d-flex align-items-center">
-                           <div class=""><i class="bi bi-speedometer"></i></div>
-                           <div class="ms-3"><span>Dashboard</span></div>
-                         </div>
-                       </a>
-                    </li>
-                    <li>
-                      <a class="dropdown-item" href="#">
-                         <div class="d-flex align-items-center">
-                           <div class=""><i class="bi bi-piggy-bank-fill"></i></div>
-                           <div class="ms-3"><span>Earnings</span></div>
-                         </div>
-                       </a>
-                    </li>
-                    <li>
-                      <a class="dropdown-item" href="#">
-                         <div class="d-flex align-items-center">
-                           <div class=""><i class="bi bi-cloud-arrow-down-fill"></i></div>
-                           <div class="ms-3"><span>Downloads</span></div>
-                         </div>
-                       </a>
-                    </li>
-                    <li><hr class="dropdown-divider"></li>
-                    <li>
-                      <a class="dropdown-item" href="{{route('logout')}}">
-                         <div class="d-flex align-items-center">
-                           <div class=""><i class="bi bi-lock-fill"></i></div>
-                           <div class="ms-3"><span>Logout</span></div>
-                         </div>
-                       </a>
+            <div class="top-navbar-right ms-auto">
+                <ul class="navbar-nav align-items-center">
+
+                    <li class="nav-item dropdown dropdown-user-setting">
+                        <a class="nav-link dropdown-toggle dropdown-toggle-nocaret" href="#"
+                            data-bs-toggle="dropdown">
+                            <div class="user-setting d-flex align-items-center">
+                                <img src="{{ Storage::url(Auth::user()->profile_image) }}" class="user-img"
+                                    alt="">
+                            </div>
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-end">
+                            <li>
+                                <a class="dropdown-item" href="#">
+                                    <div class="d-flex align-items-center">
+                                        <img src="" alt="" class="rounded-circle" width="54"
+                                            height="54">
+                                        <div class="ms-3">
+                                            <h6 class="mb-0 dropdown-user-name">{{ Auth::user()->name }}</h6>
+                                            <small
+                                                class="mb-0 dropdown-user-designation text-secondary">{{ Auth::user()->role }}</small>
+                                        </div>
+                                    </div>
+                                </a>
+                            </li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href="pages-user-profile.html">
+                                    <div class="d-flex align-items-center">
+                                        <div class=""><i class="bi bi-person-fill"></i></div>
+                                        <div class="ms-3"><span>Profile</span></div>
+                                    </div>
+                                </a>
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href="#">
+                                    <div class="d-flex align-items-center">
+                                        <div class=""><i class="bi bi-gear-fill"></i></div>
+                                        <div class="ms-3"><span>Setting</span></div>
+                                    </div>
+                                </a>
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href="index2.html">
+                                    <div class="d-flex align-items-center">
+                                        <div class=""><i class="bi bi-speedometer"></i></div>
+                                        <div class="ms-3"><span>Dashboard</span></div>
+                                    </div>
+                                </a>
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href="#">
+                                    <div class="d-flex align-items-center">
+                                        <div class=""><i class="bi bi-piggy-bank-fill"></i></div>
+                                        <div class="ms-3"><span>Earnings</span></div>
+                                    </div>
+                                </a>
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href="#">
+                                    <div class="d-flex align-items-center">
+                                        <div class=""><i class="bi bi-cloud-arrow-down-fill"></i></div>
+                                        <div class="ms-3"><span>Downloads</span></div>
+                                    </div>
+                                </a>
+                            </li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li>
+                                <a class="dropdown-item" href="{{ route('logout') }}">
+                                    <div class="d-flex align-items-center">
+                                        <div class=""><i class="bi bi-lock-fill"></i></div>
+                                        <div class="ms-3"><span>Logout</span></div>
+                                    </div>
+                                </a>
+                            </li>
+                        </ul>
                     </li>
                 </ul>
-              </li>
-              </ul>
-              </div>
+            </div>
         </nav>
-      </header>
+    </header>
 </div>
