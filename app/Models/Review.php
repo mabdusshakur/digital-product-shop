61fe74f2bdcdf529c8b30ee99b34764085e76554
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-15 14:26:09 +0600

    user relation added

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -14,4 +14,8 @@ class Review extends Model
     {
         return $this->belongsTo(Product::class);
     }
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
