1383fbf4f902592ef380f88f320dca090cf71bee
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-15 13:29:21 +0600

    update password added

--- app/Livewire/User/ProfileComponent.php
+++ app/Livewire/User/ProfileComponent.php
@@ -4,12 +4,14 @@ namespace App\Livewire\User;
 
 use App\Models\Order;
 use App\Models\OrderItem;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class ProfileComponent extends Component
 {
     public $user;
     public $name, $email, $phone_number;
+    public $current_password, $new_password, $confirm_password;
     public function mount()
     {
         $this->user = auth()->user();
@@ -44,7 +46,37 @@ class ProfileComponent extends Component
             session()->flash('success', 'Profile has been updated successfully!');
         } else {
             session()->flash('error', 'Something went wrong!');
-        }   
+        }
+    }
+    public function updatePassword()
+    {
+        $validatedData = $this->validate([
+            'current_password' => 'required',
+            'new_password' => 'required|min:6|same:confirm_password',
+            'confirm_password' => 'required',
+        ]);
+        if (!$validatedData) {
+            foreach ($validatedData as $key => $value) {
+                if ($value) {
+                    $errors[$key] = $value;
+                }
+            }
+            foreach ($errors as $key => $value) {
+                $this->addError($key, $value);
+            }
+        }
+
+        $user = auth()->user();
+        if (Auth::attempt(['email' => $user->email, 'password' => $this->current_password])) {
+            $user->password = bcrypt($this->new_password);
+            if ($user->save()) {
+                session()->flash('success', 'Password has been updated successfully!');
+            } else {
+                session()->flash('error', 'Something went wrong!');
+            }
+        } else {
+            session()->flash('error', 'Current password is incorrect!');
+        }
     }
     public function render()
     {
--- resources/views/livewire/user/profile-component.blade.php
+++ resources/views/livewire/user/profile-component.blade.php
@@ -408,26 +408,38 @@
                         <div class="row align-items-center">
                             <div class="col-lg-6">
                                 <div class="form-section">
-                                    <form action="#">
+                                    <form wire:submit.prevent="updatePassword">
                                         <div class="currentpass form-item">
-                                            <label for="currentpass" class="form-label">Current Password*</label>
-                                            <input type="password" class="form-control" id="currentpass"
-                                                placeholder="******">
+                                            <label for="current_password" class="form-label ">Current
+                                                Password*</label>
+                                            <input type="password"
+                                                class="form-control @error('current_password') is-invalid @enderror"
+                                                id="current_password" placeholder="******" wire:model="current_password">
+                                            @error('current_password')
+                                                <span class="invalid-feedback"> {{ $message }}</span>
+                                            @enderror
                                         </div>
                                         <div class="password form-item">
-                                            <label for="pass" class="form-label">Password*</label>
-                                            <input type="password" class="form-control" id="pass"
-                                                placeholder="******">
+                                            <label for="new_password" class="form-label">Password*</label>
+                                            <input type="password" class="form-control @error('new_password') is-invalid @enderror" id="new_password"
+                                                placeholder="******" wire:model="new_password">
+                                                @error('new_password')
+                                                <span class="invalid-feedback"> {{ $message }}</span>
+                                            @enderror
                                         </div>
                                         <div class="re-password form-item">
-                                            <label for="repass" class="form-label">Re-enter Password*</label>
-                                            <input type="password" class="form-control" id="repass"
-                                                placeholder="******">
+                                            <label for="confirm_password" class="form-label">Re-enter
+                                                Password*</label>
+                                            <input type="password" class="form-control @error('confirm_password') is-invalid @enderror" id="confirm_password"
+                                                placeholder="******" wire:model="confirm_password">
+                                                @error('confirm_password')
+                                                <span class="invalid-feedback"> {{ $message }}</span>
+                                            @enderror
+                                        </div>
+                                        <div class="form-btn">
+                                            <button class="shop-btn" type="submit">Upldate Password</button>
                                         </div>
                                     </form>
-                                    <div class="form-btn">
-                                        <a href="#" class="shop-btn">Upldate Password</a>
-                                    </div>
                                 </div>
                             </div>
                         </div>
