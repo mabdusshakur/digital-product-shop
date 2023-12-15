b2acbaaa39f99dd96536b1aad086b1eb1296ab6b
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-15 13:07:24 +0600

    wishlist and details button activated

--- app/Livewire/User/ProductsComponent.php
+++ app/Livewire/User/ProductsComponent.php
@@ -4,6 +4,7 @@ namespace App\Livewire\User;
 
 use App\Models\Product;
 use Livewire\Component;
+use App\Models\Wishlist;
 
 class ProductsComponent extends Component
 {
@@ -11,6 +12,23 @@ class ProductsComponent extends Component
 
     public function mount(){
         $this->products = Product::all();
+    }
+    public function productDetails($id, $slug, $category_id)
+    {
+        return redirect()->route('user.product-details', ['id' => $id, 'slug' => $slug, 'category_id' => $category_id]);
+    }
+    public function addToWishList($id)
+    {
+        if (!auth()->check()) {
+            return redirect()->route('login');
+        }
+        $Wishlist = Wishlist::create([
+            'user_id' => auth()->user()->id,
+            'product_id' => $id
+        ]);
+        if ($Wishlist) {
+           return redirect()->route('user.wishlist')->with('success', 'Item added in wishlist successfully.');
+        }
     }
     public function render()
     {
