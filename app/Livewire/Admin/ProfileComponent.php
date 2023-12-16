b738aeec2dfd0ed7cd40c9a4e9bb2e4aacdcb113
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-16 13:57:48 +0600

    information and password update added

--- app/Livewire/Admin/ProfileComponent.php
+++ app/Livewire/Admin/ProfileComponent.php
@@ -18,11 +18,53 @@ class ProfileComponent extends Component
     }
     public function updateInformation()
     {
-        
+        $validatedData = $this->validate([
+            'name' => 'required|string',
+            'email' => 'required|email',
+            'phone_number' => 'required|numeric'
+        ]);
+        if (!$validatedData) {
+            foreach ($validatedData as $key => $value) {
+                if ($value) {
+                    $errors[$key] = $value;
+                }
+            }
+            foreach ($errors as $key => $value) {
+                $this->addError($key, $value);
+            }
+        }
+
+        $user = auth()->user();
+        $user->update($validatedData);
+        session()->flash('success', 'Profile information updated successfully.');
     }
     public function updatePassword()
     {
+        $validatedData = $this->validate([
+            'current_password' => 'required',
+            'new_password' => 'required|same:confirm_password',
+            'confirm_password' => 'required'
+        ]);
+        if (!$validatedData) {
+            foreach ($validatedData as $key => $value) {
+                if ($value) {
+                    $errors[$key] = $value;
+                }
+            }
+            foreach ($errors as $key => $value) {
+                $this->addError($key, $value);
+            }
+        }
 
+        $user = auth()->user();
+        if (\Hash::check($this->current_password, $user->password)) {
+            $user->update([
+                'password' => \Hash::make($this->new_password)
+            ]);
+            session()->flash('success', 'Password updated successfully.');
+        } else {
+            session()->flash('error', 'Current password is incorrect.');
+        }
     }
     public function updateProfileImage()
     {
