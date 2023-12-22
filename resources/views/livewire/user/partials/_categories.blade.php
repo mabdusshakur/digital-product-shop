87cbd94a2ea9ceb25c4ad60c38667c4195662916
M Abdus Shakur <mabdusshakur12@gmail.com>
2023-12-22 20:16:52 +0600

    fixed image size added

--- resources/views/livewire/user/partials/_categories.blade.php
+++ resources/views/livewire/user/partials/_categories.blade.php
@@ -7,7 +7,7 @@
             @foreach ($categories as $category)
                 <div class="product-wrapper" data-aos="fade-right" data-aos-duration="200">
                     <div class="wrapper-img">
-                        <img src="{{ Storage::url($category->image) }}" alt="dress">
+                        <img src="{{ Storage::url($category->image) }}" alt="dress" height="120rem" >
                     </div>
                     <div class="wrapper-info">
                         <a href="javascript:;" wire:click="goToProductsByCategory({{$category->id}})" class="wrapper-details">{{ $category->name }}</a>
