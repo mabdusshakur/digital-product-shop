e568e684b195b82d12f50db7284a34055c0d46d0
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-01-02 16:22:43 +0600

    meta component added

---
+++ resources/views/components/meta-component.blade.php
@@ -0,0 +1,4 @@
+<title>Shopus: Your One-Stop Destination for Fashion and Style</title>
+<meta name="description" content="Shopus is your one-stop destination for fashion and style. Discover the latest trends, shop for your favorite products, and stay updated with the latest fashion news.">
+<meta name="keywords" content="fashion, style, shop, trends, clothing, accessories">
+<meta name="author" content="Shopus">
