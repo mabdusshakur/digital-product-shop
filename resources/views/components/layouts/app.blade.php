37da9986917218b6a58350ce4419b9e0150e054e
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-01-02 16:22:52 +0600

    meta component added

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -2,7 +2,28 @@
 <html lang="en">
 
 <head>
-    @livewire('user.head-component')
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="{{Storage::url('website/favicon.png')}}">
+    <x-meta-component />
+    <!--------------- swiper-css ---------------->
+    <link rel="stylesheet" href="{{ asset('resources/user/css/swiper10-bundle.min.css') }}">
+
+    <!--------------- bootstrap-css ---------------->
+    <link rel="stylesheet" href="{{ asset('resources/user/css/bootstrap-5.3.2.min.css') }}">
+
+    <!---------------------- Range Slider ------------------->
+    <link rel="stylesheet" href="{{ asset('resources/user/css/nouislider.min.css') }}">
+
+    <!---------------------- Scroll ------------------->
+    <link rel="stylesheet" href="{{ asset('resources/user/css/aos-3.0.0.css') }}">
+
+    <!--------------- additional-css ---------------->
+    <link rel="stylesheet" href="{{ asset('resources/user/css/style.css') }}">
+    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    @livewireStyles
 </head>
 
 <body>
