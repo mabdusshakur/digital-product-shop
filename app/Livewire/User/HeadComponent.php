4fe4f8e0384a9353f49d5f48ac0d9724672a75f8
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-01-02 16:16:49 +0600

    user head component added

---
+++ app/Livewire/User/HeadComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\User;
+
+use Livewire\Component;
+
+class HeadComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.user.head-component');
+    }
+}
