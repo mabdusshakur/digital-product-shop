ff9a3cc0a7b059dc180411964843a121204cf165
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-01-02 15:43:25 +0600

    sitemap product res added

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class SitemapController extends Controller
 {
-    //
+    public function index()
+    {
+        return response()->view('sitemap', ['products' => Product::orderBy('updated_at', 'desc')->get()])->header('Content-Type', 'text/xml');
+    }
 }
