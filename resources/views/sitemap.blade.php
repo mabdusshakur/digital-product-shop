3a26d18f2445c07f58122c74c1891add36d1257e
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-01-02 15:54:13 +0600

    root sitemap url added

---
+++ resources/views/sitemap.blade.php
@@ -0,0 +1,9 @@
+<?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+        <loc>{{ url('/') }}</loc>
+        <lastmod>{{ now()->toAtomString() }}</lastmod>
+        <changefreq>daily</changefreq>
+        <priority>1.0</priority>
+    </url>
+</urlset>
