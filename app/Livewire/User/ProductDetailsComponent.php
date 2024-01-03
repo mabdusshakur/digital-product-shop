9eb68a8631a59e80df77c17a36abf868c6497506
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-01-03 18:56:18 +0600

    extra add to wishlist and cart function removed

--- app/Livewire/User/ProductDetailsComponent.php
+++ app/Livewire/User/ProductDetailsComponent.php
@@ -24,45 +24,7 @@ class ProductDetailsComponent extends Component
             $this->product->increment('view_count',1);
         }
     }
-
-    public function addToWishList()
-    {
-        if (!auth()->check()) {
-            return redirect()->route('login');
-        }
-        $Wishlist = Wishlist::create([
-            'user_id' => auth()->user()->id,
-            'product_id' => $this->product->id
-        ]);
-        if ($Wishlist) {
-            return redirect()->route('user.wishlist')->with('success', 'Item added in wishlist successfully.');
-        }
-    }
-    public function addToCart()
-    {
-        if (!auth()->check()) {
-            return redirect()->route('login');
-        }
-        $cart = Cart::where('user_id', auth()->user()->id)->where('subscription_id', $this->subscription_id)->first();
-        if ($cart) {
-            $cart->quantity += $this->quantity;
-            $cart->save();
-            session()->flash('success', 'Item added in cart successfully.');
-        } else {
-            $cart = Cart::create([
-                'user_id' => auth()->user()->id,
-                'product_id' => $this->product->id,
-                'subscription_id' => $this->subscription_id,
-                'quantity' => $this->quantity
-            ]);
-            if ($cart) {
-                session()->flash('success', 'Item added in cart successfully.');
-            } else {
-                session()->flash('error', 'Something went wrong!');
-            }
-        }
-    }
-
+    
     public function addReview()
     {
         if (!auth()->check()) {
