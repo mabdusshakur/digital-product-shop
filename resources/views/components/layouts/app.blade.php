257a000bbd1607e7be8748fd0a2695f66f713ea5
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-01-03 20:50:17 +0600

    custom head,script elements component attached

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -23,6 +23,8 @@
     
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
+    <x-custom-head-element-inject/>
+
     @livewireStyles
 </head>
 
@@ -60,6 +62,10 @@
 
     <!--------------- additional-js ---------------->
     <script src="{{ asset('resources/user/assets/js/shopus.js') }}"></script>
+
+
+    <x-custom-script-element-inject/>
+
     @livewireScripts
 
 </body>
