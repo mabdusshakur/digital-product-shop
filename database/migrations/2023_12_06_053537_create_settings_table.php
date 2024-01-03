17cbd3b9d18a653227e8c4245dbb4034a982987e
M Abdus Shakur <mabdusshakur12@gmail.com>
2024-01-03 22:14:38 +0600

    currency field added

--- database/migrations/2023_12_06_053537_create_settings_table.php
+++ database/migrations/2023_12_06_053537_create_settings_table.php
@@ -25,6 +25,7 @@ return new class extends Migration
             $table->longText('privacy_policy')->nullable();
             $table->longText('custom_script_element')->nullable();
             $table->longText('custom_head_element')->nullable();
+            $table->string('currency_unicode')->nullable();
             $table->timestamps();
         });
     }
